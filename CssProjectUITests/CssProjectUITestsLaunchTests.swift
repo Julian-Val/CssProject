5993b1648e2ca7c45aa5c2884ce2ca3b8aa53ee4
Julian-Val <julianval1710@gmail.com>
2023-04-19 21:41:09 -0700

    Initial Commit

---
+++ CssProjectUITests/CssProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CssProjectUITestsLaunchTests.swift
+//  CssProjectUITests
+//
+//  Created by Julian Valencia on 4/19/23.
+//
+
+import XCTest
+
+final class CssProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
